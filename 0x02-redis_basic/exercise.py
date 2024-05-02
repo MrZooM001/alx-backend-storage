eb5bb478af792ca3a71c1c93004349ca3842930f
Hazem Magdy <hhazem0010@gmail.com>
2024-05-02 05:56:38 +0300

    Redis & mandatory tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,83 @@ Module to implement Redis
 import redis
 from typing import Union, Optional, Callable
 from uuid import uuid4
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Function to count the number of times a method is called.
+    It uses Redis to store the count for each method.
+
+    Arguments:
+        method (Callable): The method to be decorated.
+    """
+
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        """
+        The wrapper function that increments the count in Redis and calls the decorated method.
+
+        Arguemtns:
+            self (object): The instance of the class.
+            *args (tuple): The positional arguments passed to the decorated method.
+            **kwargs (dict): The keyword arguments passed to the decorated method.
+        """
+        key = method.__qualname__
+        self._redis.incr(key, 0) + 1
+        return method(self, *args, **kwargs)
+
+    return wrapped
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Function to record the input and output of a method in Redis.
+
+    Arguments:
+        method (Callable): The method to be decorated.
+    """
+
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        """
+        The wrapper function that records the input and output of the decorated method in Redis.
+
+        Arguments:
+            self (object): The instance of the class.
+            *args (tuple): The positional arguments passed to the decorated method.
+            **kwargs (dict): The keyword arguments passed to the decorated method.
+        """
+        key_func = method.__qualname__
+        input_keys = "{}:inputs".format(key_func)
+        output_keys = "{}:outputs".format(key_func)
+        self._redis.rpush(input_keys, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_keys, str(result))
+        return result
+
+    return wrapped
+
+
+def replay(self, method: Callable) -> None:
+    """
+    Replays the input and output of a method
+    that has been decorated with the `call_history` decorator.
+
+    Arguments:
+        method (Callable): The method to be replayed.
+        This method should have been decorated with the `call_history` decorator.
+    """
+    key_func = method.__qualname__
+    input_keys = "{}:inputs".format(key_func)
+    output_keys = "{}:outputs".format(key_func)
+    inputs = self._redis.lrange(input_keys, 0, -1)
+    outputs = self._redis.lrange(output_keys, 0, -1)
+    print("{} was called {} times:".format(key_func, len(inputs)))
+    for inp, out in zip(inputs, outputs):
+        print(
+            "{}(*{}) -> {}".format(key_func, inp.decode("utf-8"), out.decode("utf-8"))
+        )
 
 
 class Cache:
